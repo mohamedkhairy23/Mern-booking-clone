c0d677f04c6c27ec0ddb471ce0d46c075f3d3af6
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-01-02 22:50:17 +0200

    automation testing for adding new hotel by user to database

--- frontend/src/pages/AddHotel.tsx
+++ frontend/src/pages/AddHotel.tsx
@@ -2,16 +2,16 @@ import { useMutation } from "react-query";
 import ManageHotelForm from "../forms/ManageHtelForm/ManageHotelForm";
 import { useAppContext } from "../contexts/AppContext";
 import * as apiClient from "../api-client";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 const AddHotel = () => {
   const { showToast } = useAppContext();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const { mutate, isLoading } = useMutation(apiClient.addMyHotel, {
     onSuccess: () => {
       showToast({ message: "Hotel Saved!", type: "SUCCESS" });
-      navigate("/my-hotels");
+      // navigate("/my-hotels");
     },
     onError: () => {
       showToast({ message: "Error Saving Hotel", type: "ERROR" });
