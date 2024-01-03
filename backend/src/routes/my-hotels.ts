29607f3afa50a9f32889098d17df18ffd6e93ce6
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-01-03 14:52:56 +0200

    endpoint for get my hotels

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -64,4 +64,13 @@ router.post(
   }
 );
 
+router.get("/", verifyToken, async (req: Request, res: Response) => {
+  try {
+    const hotels = await Hotel.find({ userId: req.userId });
+    res.status(200).json(hotels);
+  } catch (error: any) {
+    res.status(500).json({ message: "Error fetching hotels" });
+  }
+});
+
 export default router;
