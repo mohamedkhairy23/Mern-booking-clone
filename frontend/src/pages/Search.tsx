c2f6d0ef83c9ad34198bcd678c2c2c9ffee0d3dd
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-01-05 13:13:25 +0200

    search page

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,10 +1,51 @@
+import { useQuery } from "react-query";
 import { useSearchContext } from "../contexts/SearchContext";
+import * as apiClient from "../api-client";
+import { useState } from "react";
+import SearchResultCard from "../components/SearchResultCard";
+import { HotelType } from "../../../backend/src/shared/types";
 
 const Search = () => {
   const search = useSearchContext();
-  console.log(search);
+  const [page, setPage] = useState<number>(1);
 
-  return <div>Search Page</div>;
+  const searchParams = {
+    destination: search.destination,
+    checkIn: search.checkIn.toISOString(),
+    checkOut: search.checkOut.toISOString(),
+    adultCount: search.adultCount.toString(),
+    childCount: search.childCount.toString(),
+    page: page.toString(),
+  };
+
+  const { data: hotelData } = useQuery(["searchHotels", searchParams], () =>
+    apiClient.SearchHotels(searchParams)
+  );
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5 mx-3">
+      <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
+        <div className="space-y-5">
+          <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
+            Filter By:
+          </h3>
+          {/* TODO Filters */}
+        </div>
+      </div>
+      <div className="flex flex-col gap-5">
+        <div className="flex justify-between items-center">
+          <span className="text-xl font-bold">
+            {hotelData?.pagination.total} Hotels found
+            {search.destination ? `in ${search.destination}` : ""}
+          </span>
+          {/* TODO sort options */}
+        </div>
+        {hotelData?.data.map((hotel: HotelType) => (
+          <SearchResultCard hotel={hotel} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
