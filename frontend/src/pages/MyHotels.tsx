2141a9bfa8d0d73cda9a3ebeb58494b3cffbcf60
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-01-05 22:26:00 +0200

    adding spinner

--- frontend/src/pages/MyHotels.tsx
+++ frontend/src/pages/MyHotels.tsx
@@ -3,15 +3,16 @@ import { Link } from "react-router-dom";
 import * as apiClient from "../api-client";
 import { BsBuilding, BsMap } from "react-icons/bs";
 import { BiHotel, BiMoney, BiStar } from "react-icons/bi";
+import Spinner from "../components/Spinner";
 
 const MyHotels = () => {
-  const { data: hotelData } = useQuery(
-    "fetchMyHotels",
-    apiClient.fetchMyHotels,
-    {
-      onError: () => {},
-    }
-  );
+  const {
+    isLoading,
+    isError,
+    data: hotelData,
+  } = useQuery("fetchMyHotels", apiClient.fetchMyHotels, {
+    onError: () => {},
+  });
 
   if (!hotelData) {
     return <span>No Hotels Found</span>;
@@ -30,46 +31,56 @@ const MyHotels = () => {
       </span>
 
       <div className="grid grid-cols-1 gap-8">
-        {hotelData?.map((hotel) => (
-          <div
-            key={hotel._id}
-            data-testid="hotel-card"
-            className="flex flex-col justify-between border border-slate-300 rounded-lg p-8 gap-5"
-          >
-            <h2 className="text-2xl font-bold">{hotel.name}</h2>
-            <div className="whitespace-pre-line">{hotel.description}</div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-2">
-              <div className="border border-slate-300 rounded-sm p-3 flex items-center">
-                <BsMap className="mr-1" />
-                {hotel.city}, {hotel.country}
+        {isLoading ? (
+          <>
+            <Spinner />
+          </>
+        ) : isError ? (
+          <>
+            <p>Something went wrong</p>
+          </>
+        ) : (
+          hotelData?.map((hotel) => (
+            <div
+              key={hotel._id}
+              data-testid="hotel-card"
+              className="flex flex-col justify-between border border-slate-300 rounded-lg p-8 gap-5"
+            >
+              <h2 className="text-2xl font-bold">{hotel.name}</h2>
+              <div className="whitespace-pre-line">{hotel.description}</div>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-2">
+                <div className="border border-slate-300 rounded-sm p-3 flex items-center">
+                  <BsMap className="mr-1" />
+                  {hotel.city}, {hotel.country}
+                </div>
+                <div className="border border-slate-300 rounded-sm p-3 flex items-center">
+                  <BsBuilding className="mr-1" />
+                  {hotel.type}
+                </div>
+                <div className="border border-slate-300 rounded-sm p-3 flex items-center">
+                  <BiMoney className="mr-1" />
+                  {`£${hotel.pricePerNight} per night`}
+                </div>
+                <div className="border border-slate-300 rounded-sm p-3 flex items-center">
+                  <BiHotel className="mr-1" />
+                  {hotel.adultCount} adults, {hotel.childCount} children
+                </div>{" "}
+                <div className="border border-slate-300 rounded-sm p-3 flex items-center">
+                  <BiStar className="mr-1" />
+                  {hotel.starRating} Star Rating
+                </div>
               </div>
-              <div className="border border-slate-300 rounded-sm p-3 flex items-center">
-                <BsBuilding className="mr-1" />
-                {hotel.type}
-              </div>
-              <div className="border border-slate-300 rounded-sm p-3 flex items-center">
-                <BiMoney className="mr-1" />
-                {`£${hotel.pricePerNight} per night`}
-              </div>
-              <div className="border border-slate-300 rounded-sm p-3 flex items-center">
-                <BiHotel className="mr-1" />
-                {hotel.adultCount} adults, {hotel.childCount} children
-              </div>{" "}
-              <div className="border border-slate-300 rounded-sm p-3 flex items-center">
-                <BiStar className="mr-1" />
-                {hotel.starRating} Star Rating
-              </div>
+              <span className="flex justify-end">
+                <Link
+                  className="flex bg-blue-600 text-white text-xl font-bold p-2 hover:bg-blue-500"
+                  to={`/edit-hotel/${hotel._id}`}
+                >
+                  View Details
+                </Link>
+              </span>
             </div>
-            <span className="flex justify-end">
-              <Link
-                className="flex bg-blue-600 text-white text-xl font-bold p-2 hover:bg-blue-500"
-                to={`/edit-hotel/${hotel._id}`}
-              >
-                View Details
-              </Link>
-            </span>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
