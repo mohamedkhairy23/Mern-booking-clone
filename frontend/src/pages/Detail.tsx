e090a5a8a1ea87beeaa932caad2f758bf75ef9de
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-01-06 02:31:42 +0200

    adding spinner

--- frontend/src/pages/Detail.tsx
+++ frontend/src/pages/Detail.tsx
@@ -3,11 +3,16 @@ import { useParams } from "react-router-dom";
 import * as apiClient from "../api-client";
 import { AiFillStar } from "react-icons/ai";
 import GuestInfoForm from "../forms/GuestInfoForm/GuestInfoForm";
+import Spinner from "../components/Spinner";
 
 const Detail = () => {
   const { hotelId } = useParams();
 
-  const { data: hotel } = useQuery(
+  const {
+    isLoading,
+    isError,
+    data: hotel,
+  } = useQuery(
     "fetchHotelById",
     () => apiClient.fetchHotelById(hotelId as string),
     {
@@ -21,44 +26,56 @@ const Detail = () => {
 
   return (
     <div className="space-y-6 mx-3">
-      <div>
-        <span className="flex">
-          {Array.from({ length: hotel.starRating }).map(() => (
-            <AiFillStar className="fill-yellow-400" />
-          ))}
-        </span>
-        <h1 className="text-3xl font-bold">{hotel.name}</h1>
-      </div>
-
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-        {hotel.imageUrls.map((image: string) => (
-          <div className="h-[300px]">
-            <img
-              src={image}
-              alt={hotel.name}
-              className="rounded-md w-full h-full object-cover object-center"
-            />
+      {isLoading ? (
+        <>
+          <Spinner />
+        </>
+      ) : isError ? (
+        <>
+          <p>Something went wrong</p>
+        </>
+      ) : (
+        <>
+          <div>
+            <span className="flex">
+              {Array.from({ length: hotel.starRating }).map(() => (
+                <AiFillStar className="fill-yellow-400" />
+              ))}
+            </span>
+            <h1 className="text-3xl font-bold">{hotel.name}</h1>
           </div>
-        ))}
-      </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-4 gap-2">
-        {hotel.facilities.map((facility) => (
-          <div className="border border-slate-300 rounded-sm p-3">
-            {facility}
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+            {hotel.imageUrls.map((image: string) => (
+              <div className="h-[300px]">
+                <img
+                  src={image}
+                  alt={hotel.name}
+                  className="rounded-md w-full h-full object-cover object-center"
+                />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-[2fr_1fr]">
-        <div className="whitespace-pre-line">{hotel.description}</div>
-        <div className="h-fit">
-          <GuestInfoForm
-            hotelId={hotel._id}
-            pricePerNight={hotel.pricePerNight}
-          />
-        </div>
-      </div>
+          <div className="grid grid-cols-1 lg:grid-cols-4 gap-2">
+            {hotel.facilities.map((facility) => (
+              <div className="border border-slate-300 rounded-sm p-3">
+                {facility}
+              </div>
+            ))}
+          </div>
+
+          <div className="grid grid-cols-1 lg:grid-cols-[2fr_1fr]">
+            <div className="whitespace-pre-line">{hotel.description}</div>
+            <div className="h-fit">
+              <GuestInfoForm
+                hotelId={hotel._id}
+                pricePerNight={hotel.pricePerNight}
+              />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
