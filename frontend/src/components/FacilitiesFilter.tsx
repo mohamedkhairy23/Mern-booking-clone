c813b0742044412233c95e04b275f2f694c3f788
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-01-07 20:45:49 +0200

    commit

--- frontend/src/components/FacilitiesFilter.tsx
+++ frontend/src/components/FacilitiesFilter.tsx
@@ -9,7 +9,7 @@ type Props = {
 const FacilitiesFilter = ({ selectedFacilities, onChange }: Props) => {
   return (
     <div className="border-b border-slate-300 pb-5">
-      <h4 className="text-md font-semibold mb-2">Hotel Type</h4>
+      <h4 className="text-md font-semibold mb-2">Facilities</h4>
       {hotelFacilities.map((facility) => (
         <label className="flex items-center space-x-2">
           <input
